ad129d41f480f6d2251860981204af083d6b3ca1
Philip Huysmans <philip.huysmans@gmail.com>
2017-02-18 22:10:04 +0100

    initial commit

---
+++ CardListener.py
@@ -0,0 +1,21 @@
+import binascii
+import sys
+
+import Adafruit_PN532 as PN532
+
+class CardListener:
+    def __init__(self):
+        CS   = 18
+        MOSI = 23
+        MISO = 24
+        SCLK = 25
+
+        self.pn532 = PN532.PN532(cs=CS, sclk=SCLK, mosi=MOSI, miso=MISO)
+        self.pn532.begin()
+        print('Found PN532 with firmware version: {0}.{1}'.format(ver, rev))
+
+        self.pn532.SAM_configuration()
+
+    def checkCard(self):
+        uid = self.pn532.read_passive_target()
+        return format(binascii.hexlify(uid))
---
+++ app.py
@@ -0,0 +1,22 @@
+cardListener = CardListener()
+redisClient = RedisClient()
+mopidyClient = MopidyClient()
+
+cachedCardId
+
+while True:
+    cardId = cardListener.checkCard()
+    if cardId is None:
+        continue
+
+    if cardId == cachedCardId:
+        continue
+
+    cachedCardId = cardId
+
+    print('Got new card with UID: 0x{0}'.cardId
+
+    # Based on the card id, something can be fetched from REDIS
+    albumId = redisClient.retrieve(cardId)
+
+    # Based on Redis output, mopidy can be called

c20f4a5cd8af13332b2499a00ccae0f4fcbd345d
Philip <philip.huysmans@gmail.com>
2017-03-21 20:29:22 +0000

    test

--- MopidyClient.py
+++ MopidyClient.py
@@ -5,9 +5,10 @@ class MopidyClient:
         self.clearData = '{ "jsonrpc": "2.0", "id": 1, "method": "core.tracklist.clear" }'
         self.playData = '{ "jsonrpc": "2.0", "id": 1, "method": "core.playback.play" }'
         self.stopData = '{ "jsonrpc": "2.0", "id": 1, "method": "core.playback.stop" }'
-        self.url = 'http://192.168.1.107:6680/mopidy/rpc'
+        self.url = 'http://127.0.0.1:6680/mopidy/rpc'
 
     def play(self,id):
+	print("playing" + id);
         loadData = '{ "jsonrpc": "2.0", "id": 1, "method": "core.tracklist.add", "params": { "uri": "' + id + '" } }'
         requests.post(self.url, data=self.clearData)
         requests.post(self.url, data=loadData)

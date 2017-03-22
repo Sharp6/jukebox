eb64b69e7710f98b33620c869d10f65f40c81c1b
Huysmans Philip <phuysman@ad.ua.ac.be>
2017-03-22 13:53:27 +0100

    added sockets to configserver

--- ConfigServer.py
+++ ConfigServer.py
@@ -1,18 +1,31 @@
-from flask import Flask
-import thread
+from flask import Flask, render_template, jsonify
+from flask_socketio import SocketIO
+import _thread
 
 class ConfigServer():
     def __init__(self):
-        self.data = 'foo'
         self.app = Flask(__name__)
+        self.app.config['SECRET_KEY'] = 'secret!'
         self.app.add_url_rule('/', 'main', self.main)
 
+        self.socketio = SocketIO(self.app)
+
     def main(self):
-        return self.data
+        return render_template('index.html')
+
+    def setCard(self, cardId):
+        emit('setCard', jsonify({ cardId: cardId }))
 
     def flaskThread(self):
-        self.app.run()
+        self.socketio.run(self.app)
 
     def run(self):
         print("Starting")
-        thread.start_new_thread(self.flaskThread,())
+        _thread.start_new_thread(self.flaskThread,())
+
+if __name__ == '__main__':
+    print("Running standalone")
+    myCS = ConfigServer()
+    myCS.run()
+    while True:
+        continue

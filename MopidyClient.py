e6d9351418427dc895e568f966ed528c21e4f079
Philip Huysmans <philip.huysmans@gmail.com>
2017-03-24 09:05:20 +0100

    added mopidy track capabilities

--- MopidyClient.py
+++ MopidyClient.py
@@ -5,26 +5,31 @@ class MopidyClient:
         self.clearData = '{ "jsonrpc": "2.0", "id": 1, "method": "core.tracklist.clear" }'
         self.playData = '{ "jsonrpc": "2.0", "id": 1, "method": "core.playback.play" }'
         self.stopData = '{ "jsonrpc": "2.0", "id": 1, "method": "core.playback.stop" }'
-        self.setVolume = '{ "jsonrpc": "2.0", "id": 1, "method": "core.mixer.set_volume", "params": { "volume": 50} }'
+        self.setVolume = '{ "jsonrpc": "2.0", "id": 1, "method": "core.mixer.set_volume", "params": { "volume": 10} }'
         self.url = 'http://127.0.0.1:6680/mopidy/rpc'
 
         requests.post(self.url, data=self.setVolume)
 
-    def play(self,id):
-	print("playing" + id);
-        print "MOPIDY PLAYING"
-        #requests.post(self.url, data=self.clearData)
-        getTracks = '{ "jsonrpc": "2.0", "id": 1, "method": "core.playlists.lookup", "params": { "uri": "m3u:handjesdraaien.m3u8" } }'
+    def play(self,uri):
+	    print("MOPIDY PLAYING " + uri)
+        requests.post(self.url, data=self.clearData)
+
+        type = uri.split(:)[0]
+
+        if type == "file":
+            print "Loading file"
+            loadUri = uri
+
+        # Currently only loads the first track of a playlist
+        if type == "m3u":
+            print "Loading M3U"
+            getTracks = '{ "jsonrpc": "2.0", "id": 1, "method": "core.playlists.lookup", "params": { "uri": "' + uri + '" } }'
+            r = requests.post(self.url, data=getTracks)
+            j = r.json()
+            loadUri = j['result']['tracks'][0]['uri']
 
         requests.post(self.url, data=self.clearData)
-        r = requests.post(self.url, data=getTracks)
-        j = r.json()
-        print "GOT JSON"
-        print j
-
-        tracks = j['result']['tracks'][0]['uri']
-
-        loadData = '{ "jsonrpc": "2.0", "id": 1, "method": "core.tracklist.add", "params": { "uri": "' + tracks + '" } }'
+        loadData = '{ "jsonrpc": "2.0", "id": 1, "method": "core.tracklist.add", "params": { "uri": "' + loadUri + '" } }'
         requests.post(self.url, data=loadData)
         requests.post(self.url, data=self.playData)
 

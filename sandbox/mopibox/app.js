5a04f4184fde5a9b91dcaebf3f568a9f997c2acd
Philip on pi <philip.huysmans@gmail.com>
2017-07-03 18:52:08 +0000

    bugs

--- sandbox/mopibox/app.js
+++ sandbox/mopibox/app.js
@@ -1,12 +1,15 @@
 var Mopidy = require("mopidy");
-var mopidy = new Mopidy();
+var mopidy = new Mopidy({
+	webSocketUrl: "ws://localhost:6680/mopidy/ws/"
+});
 
-mopidy.on(console.log.bind(console));
+//mopidy.on(console.log.bind(console));
 
 mopidy.on("state:online", function () {
     mopidy.library.browse("file:///home/pi/music")
         .then(result => {
             console.log(result);
+	    return mopidy.playback.play
         })
         .catch(console.error.bind(console))
         .done();

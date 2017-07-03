ced810c450869f61ea3524e7a5715ade5ab99c0c
Philip Huysmans <philip.huysmans@gmail.com>
2017-07-03 20:56:35 +0200

    test

--- sandbox/mopibox/app.js
+++ sandbox/mopibox/app.js
@@ -6,10 +6,13 @@ var mopidy = new Mopidy({
 //mopidy.on(console.log.bind(console));
 
 mopidy.on("state:online", function () {
-    mopidy.library.browse("file:///home/pi/music")
+    mopidy.tracklist.clear()
+        .then(() => {
+            return mopidy.library.browse("file:///home/pi/music");
+        })    
         .then(result => {
             console.log(result);
-	    return mopidy.playback.play
+	        return mopidy.tracklist.add(undefined,undefined,result[0].uri);
         })
         .catch(console.error.bind(console))
         .done();

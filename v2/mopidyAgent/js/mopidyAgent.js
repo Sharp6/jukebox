a02659789235b742a1f6d5eb33bbdd2704ffe038
Philip Huysmans <philip.huysmans@gmail.com>
2017-07-09 22:47:06 +0200

    bugfix

--- v2/mopidyAgent/js/mopidyAgent.js
+++ v2/mopidyAgent/js/mopidyAgent.js
@@ -2,6 +2,7 @@ var Worker = require('./mopidyAgentWorker');
 var Mopidy = require("mopidy");
 var amqp = require('amqplib/callback_api');
 var RabbitSender = require('./mopidyAgentRabbitSender');
+var RabbitListener = require('./mopidyAgentRabbitListener');
 
 var mopidy = new Mopidy({
 	webSocketUrl: "ws://localhost:6680/mopidy/ws/"
@@ -9,3 +10,4 @@ var mopidy = new Mopidy({
 
 var sender = new RabbitSender(amqp);
 var worker = new Worker(mopidy, sender);
+var listener = new RabbitListener(amqp, worker);
